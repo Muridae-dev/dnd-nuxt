e53259c537cdc671477c910e2915b070b3654d6d
Muridae-dev <92300249+Muridae-dev@users.noreply.github.com>
2025-05-06 13:52:23 +0200

    inital setup complete

---
+++ components/navigation/NavHeader.vue
@@ -0,0 +1,49 @@
+<template>
+  <header>
+    <button class="menu-button" v-for="action in headerActions">
+      {{ action.title }}
+    </button>
+  </header>
+</template>
+
+<script setup lang="ts">
+const headerActions = [
+  {
+    title: "character",
+  },
+  {
+    title: "inventory",
+  },
+];
+</script>
+
+<style lang="scss">
+header {
+  height: 50px;
+  width: calc(100% - $side-spacing * 2);
+  border-radius: 5px;
+
+  padding: 0px $side-spacing;
+
+  position: fixed;
+  top: $side-spacing;
+  left: 50%;
+  transform: translateX(-50%);
+
+  background: white;
+  border: 1px solid black;
+
+  display: flex;
+
+  align-items: center;
+  gap: 10%;
+
+  .menu-button {
+    font-weight: bold;
+    text-transform: capitalize;
+    font-size: 1.2rem;
+
+    font-family: "Courier New", Courier, monospace;
+  }
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="background-noise"></div>
+    <NavigationNavHeader />
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+.background-noise {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 9999;
+
+  background: linear-gradient(hsla(0, 6%, 7%, 0) 50%, rgba(0, 0, 0, 0.1) 0),
+    linear-gradient(
+      90deg,
+      rgba(255, 0, 0, 0.06),
+      rgba(0, 255, 0, 0.02),
+      rgba(0, 0, 0, 0.26)
+    );
+  background-size: 100% 2px, 3px 100%;
+  pointer-events: none;
+}
+</style>
